d10eecd2512cd688de86623a8073a00ce01e5306
EungGu Yun <yuneg11@gmail.com>
2023-01-15 17:17:55 +0900

    Set TPU name automatically

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ Run following command on your TPU VM.
 
 ```bash
 export TOKEN=<YOUR_GITHUB_TOKEN>
-export HOSTNAME=<TPU_NAME>
 sh -c "$(curl -fsSL -H "Authorization: token $TOKEN" https://raw.githubusercontent.com/yuneg11/Cloud-TPU-Setup/master/setup.sh)"
 ```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # TOKEN=$1  # Directly set from outside
 REPO_HOME=https://raw.githubusercontent.com/yuneg11/Cloud-TPU-Setup/master/
+TPU_NAME=$(curl -s -H 'Metadata-Flavor: Google' -X GET http://metadata.google.internal/computeMetadata/v1/instance/description)
+HOSTNAME=${TPU_NAME^^}
 
 cd $HOME
 
@@ -40,14 +42,3 @@ sed -i "s/TPU_NAME=/TPU_NAME=\"$HOSTNAME\"/g" $HOME/.zshrc
 
 # Timezone
 sudo timedatectl set-timezone 'Asia/Seoul'
-
-
-# PyTorch (Specify TPU version instead)
-# sudo bash /var/scripts/docker-login.sh
-# sudo docker rm libtpu || true
-# sudo docker create --name libtpu gcr.io/cloud-tpu-v2-images/libtpu:pytorch-1.9 "/bin/bash"
-# sudo docker cp libtpu:libtpu.so /lib
-# sudo pip3 uninstall --yes torch torch_xla torchvision
-# sudo pip3 install torch==1.12
-# sudo pip3 install torchvision==0.13
-# sudo pip3 install https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-1.12-cp38-cp38-linux_x86_64.whl
